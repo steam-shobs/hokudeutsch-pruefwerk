6738d6a395d81cb6ac21fb9f6d16ecfd15cf44ed
steam-shobs <kekke0728@icloud.com>
2026-09-10 11:44:15 +0900

    Cache custom app icons

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "pruefwerk-v5";
-const ASSETS = ["./", "./index.html", "./styles.css?v=20260910-4", "./expanded-questions.js?v=20260910-4", "./app.js?v=20260910-4", "./manifest.webmanifest"];
+const CACHE = "pruefwerk-v6";
+const ASSETS = ["./", "./index.html", "./styles.css?v=20260910-5", "./expanded-questions.js?v=20260910-5", "./app.js?v=20260910-5", "./manifest.webmanifest", "./assets/favicon-64.png", "./assets/apple-touch-icon.png", "./assets/icon-192.png", "./assets/icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
